54defb7d7c5fea2958b6e602a6e939ae1039d52a
Yi Xun <myixun007@gmail.com>
2026-08-20 18:37:12 +0800

    Add greeting

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -1,10 +1,16 @@
 public class Milo {
     public static void main(String[] args) {
-        String banner = " __  __   _       \n"
+        String banner = "____________________________________________________________\n"
+                + " __  __   _       \n"
                 + "|  \\/  (_) | ___  \n"
                 + "| |\\/| | | |/ _ \\ \n"
                 + "| |  | | | | (_) |\n"
-                + "|_|  |_|_|_|\\___/\n";
+                + "|_|  |_|_|_|\\___/\n\n"
+                + "Hey there! My name is Milo.\n"
+                + "How can I help you today?\n"
+                + "____________________________________________________________\n\n"
+                + "Bye bye. Hope to see you soon!\n"
+                + "____________________________________________________________\n";
         System.out.println(banner);
     }
 }
