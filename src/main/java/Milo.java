f338c91f7c9622829ac3754c530ae60e7236d0ff
Yi Xun <myixun007@gmail.com>
2026-08-20 18:22:20 +0800

    Rename Duke.java to Milo.java

---
+++ src/main/java/Milo.java
@@ -0,0 +1,10 @@
+public class Milo {
+    public static void main(String[] args) {
+        String banner = " __  __   _       \n"
+                + "|  \\/  (_) | ___  \n"
+                + "| |\\/| | | |/ _ \\ \n"
+                + "| |  | | | | (_) |\n"
+                + "|_|  |_|_|_|\\___/\n";
+        System.out.println(banner);
+    }
+}
