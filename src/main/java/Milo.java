df40984559dc16ba7368ad0390244951c2380751
Yi Xun <myixun007@gmail.com>
2026-08-20 21:11:04 +0800

    Add echo feature

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -1,16 +1,41 @@
+import java.util.Scanner;
+
 public class Milo {
-    public static void main(String[] args) {
-        String banner = "____________________________________________________________\n"
-                + " __  __   _       \n"
-                + "|  \\/  (_) | ___  \n"
-                + "| |\\/| | | |/ _ \\ \n"
-                + "| |  | | | | (_) |\n"
-                + "|_|  |_|_|_|\\___/\n\n"
-                + "Hey there! My name is Milo.\n"
-                + "How can I help you today?\n"
-                + "____________________________________________________________\n\n"
-                + "Bye bye. Hope to see you soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(banner);
+    private static String LINE = "____________________________________________________________";
+    private static String BANNER =
+            " __  __   _       \n"
+            + "|  \\/  (_) | ___  \n"
+            + "| |\\/| | | |/ _ \\ \n"
+            + "| |  | | | | (_) |\n"
+            + "|_|  |_|_|_|\\___/\n";
+
+    public static void printResponse(String string) {
+        System.out.println(LINE + "\n" + "    " + string + "\n" + LINE + "\n");
     }
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println(BANNER);
+        System.out.println("Hey there! My name is Milo.\n"
+                + "How can I help you today?\n"
+                + LINE
+                + "\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                printResponse("Bye bye. Hope to see you soon!");
+                break;
+            } else {
+                printResponse(input);
+            }
+        }
+
+        scanner.close();
+
+    }
+
 }
