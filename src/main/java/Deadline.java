6d4224ff2048f1bb35fb277a821f5a674c2e736b
Yi Xun <myixun007@gmail.com>
2026-08-21 11:39:31 +0800

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    private String deadlineDate;
+
+    public Deadline(String description, String deadlineDate) {
+        super(description);
+        this.deadlineDate = deadlineDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D] %s (by: %s)", super.toString(), this.deadlineDate);
+    }
+ }
